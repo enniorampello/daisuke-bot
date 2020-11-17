bfe316330997ec9e04573bb4510310be8930a3cd
ennio <enniorampello5@gmail.com>
2020-11-17 14:18:06 +0100

    files arrangement

---
+++ lib/__init__.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.common.exceptions import NoSuchElementException
+from keys import *
+from time import sleep
---
+++ polito.py
@@ -0,0 +1,34 @@
+from scraper import Scraper
+from lib import *
+
+class Polito(Scraper):
+    material_urls = {
+        'sap': 'https://didattica.polito.it/portal/pls/portal/sviluppo.pagina_corso.main?t=3&p_sottopagina=VC_6718',
+        'os': 'https://didattica.polito.it/portal/pls/portal/sviluppo.pagina_corso.main?t=3&p_sottopagina=VC_7181',
+        'estm': 'https://didattica.polito.it/portal/pls/portal/sviluppo.pagina_corso.main?t=3&p_sottopagina=VC_6875',
+        'cn': 'https://didattica.polito.it/portal/pls/portal/sviluppo.pagina_corso.main?t=3&p_sottopagina=VC_6577'
+    }
+    
+    def __init__(self, parameter_list):
+        self.polito_login()
+        pass
+
+    def polito_login(self):
+        self.driver.get('https://idp.polito.it/idp/x509mixed-login')
+        self.wait.until(presence_of_element_located((By.ID, 'j_username')))
+        self.driver.find_element_by_id('j_username').send_keys(get_username())
+        self.driver.find_element_by_id('j_password').send_keys(get_password())
+        sleep(1)
+        self.driver.find_element_by_id('usernamepassword').click()
+        self.wait.until(presence_of_element_located((By.XPATH, '//*[@id="table_portali"]/div[2]/div/a')))
+        self.driver.find_element_by_xpath('//*[@id="table_portali"]/div[2]/div/a').click()
+        try:
+            popup = self.driver.find_element_by_xpath('//*[@id="myModalRemindAbb"]/div/div/div[3]/a[4]')
+        except NoSuchElementException:
+            pass
+        else:
+            popup.click()
+        sleep(5)
+
+    
+
